f01718f18b8a9d7d3ebab9c84e71d96899a0b33b
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-08-01 22:08:17 +0200

    feat(commits_fetcher): add all the repo not only railess

--- get_last_commits.py
+++ get_last_commits.py
@@ -1,24 +1,36 @@
 import requests
 from datetime import datetime
 import pytz
+import os
 
 GITHUB_API_URL = "https://api.github.com"
-REPO = "mathisbukowski/Railess"
+USERNAME = "mathisbukowski"
+GITHUB_TOKEN = os.getenv('GH_TOKEN')
 paris_tz = pytz.timezone('Europe/Paris')
 now = datetime.now(paris_tz)
 
-def fetch_commits():
-    url = f"{GITHUB_API_URL}/repos/{REPO}/commits"
-    response = requests.get(url)
+def fetch_repositories():
+    url = f"{GITHUB_API_URL}/user/repos"
+    headers = {'Authorization': f'token {GITHUB_TOKEN}'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        repos = response.json()
+        return repos
+    else:
+        response.raise_for_status()
+
+def fetch_commits(repo_name):
+    url = f"{GITHUB_API_URL}/repos/{USERNAME}/{repo_name}/commits"
+    headers = {'Authorization': f'token {GITHUB_TOKEN}'}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         commits = response.json()
         return commits[:5]
     else:
-        return []
+        response.raise_for_status()
 
-def update_readme(commits):
+def update_readme(commits_by_repo):
     readme_path = "README.md"
-
     time = now.strftime("%H:%M:%S")
 
     try:
@@ -27,16 +39,18 @@ def update_readme(commits):
     except FileNotFoundError:
         current_content = ""
 
-    new_commits_content = "\n\n## 🚦 Last commits on Railess\n\n"
-    for commit in commits:
-        message = commit['commit']['message']
-        author = commit['commit']['author']['name']
-        date = commit['commit']['author']['date']
-        new_commits_content += f"\n\n🔸 - {message} from {author} at {date}\n"
+    new_commits_content = "\n\n## 🚦 Last commits on all repositories\n\n"
+    for repo_name, commits in commits_by_repo.items():
+        new_commits_content += f"\n### {repo_name}\n"
+        for commit in commits:
+            message = commit['commit']['message']
+            author = commit['commit']['author']['name']
+            date = commit['commit']['author']['date']
+            new_commits_content += f"\n🔸 - {message} from {author} at {date}\n"
 
-    time_sentence = f"\n\n ⏲ Updated at {time}"
-    if "## 🚦 Last commits on Railess" in current_content:
-        updated_content = current_content.split("## 🚦 Last commits on Railess")[0] + new_commits_content + time_sentence
+    time_sentence = f"\n\n⏲ Updated at {time}"
+    if "## 🚦 Last commits on all repositories" in current_content:
+        updated_content = current_content.split("## 🚦 Last commits on all repositories")[0] + new_commits_content + time_sentence
     else:
         updated_content = current_content + new_commits_content + time_sentence
 
@@ -44,5 +58,9 @@ def update_readme(commits):
         readme_file.write(updated_content)
 
 if __name__ == "__main__":
-    commits = fetch_commits()
-    update_readme(commits)
+    repositories = fetch_repositories()
+    commits_by_repo = {}
+    for repo in repositories:
+        repo_name = repo['name']
+        commits_by_repo[repo_name] = fetch_commits(repo_name)
+    update_readme(commits_by_repo)
